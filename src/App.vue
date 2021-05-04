a5887b62d625d8aaeaeb1dd6f8758b12994f8eac
PATWARY <md.hasan.69q@gmail.com>
2021-05-04 19:23:08 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HadithWorld :items="items" msg="হাদিস"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HadithWorld from './components/HadithWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HadithWorld
+  },
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    fetch('https://bn-hadith-api.herokuapp.com/hadiths')
+    .then(response => response.json())
+    .then(data => {
+      this.items = data;
+    });
+  },
 }
 </script>
 
@@ -21,6 +33,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 30px 0;
 }
 </style>
---
+++ src/components/HadithWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hadith">
+    <h1>{{ msg }}</h1>
+    <ol class="hadith-list">
+      <li v-for="item in items" :key="item.id" class="hadith-list-item">
+          <span class="name"><strong>Name:</strong> {{ item.name }}</span>
+          <span class="description"><strong>Description:</strong> {{ item.description }}</span>
+          <span class="references"><strong>References:</strong> {{ item.references }}</span>
+          <span class="grade"><strong>Grade:</strong> {{ item.grade }}</span>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+    // props: ['items'],
+    name: 'HadithWorld',
+    props: {
+        msg: String,
+        items: Array
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1 {
+        font-weight: 700;
+    }
+    .hadith {
+        max-width: 900px;
+        margin: 0 auto;
+    }
+
+    .hadith-list {
+        /* list-style-type: none; */
+        padding: 0;
+        margin: 0;
+    }
+
+    .hadith-list li {
+        border: 1px solid #666;
+        padding: 10px 16px;
+        border-radius: 5px;
+        text-align: left;
+        margin-bottom: 16px;
+    }
+
+    .hadith-list li:not(:last-child) {
+        margin-bottom: 16px;
+    }
+
+    .hadith-list li > span {
+        display: block;
+        padding: 5px 0;
+    }
+</style>
